9b71b82ac45fa0ede4d06a410b53d5648a69cff5
Ilia <ilia.larchenko@gmail.com>
2024-04-21 22:32:43 +0700

    Simplified fixed messages

--- resources/data.py
+++ resources/data.py
@@ -22,11 +22,8 @@ topics_list = [
 ]
 
 fixed_messages = {
-    "intro": "Welcome to the coding interview! I am your AI interview assistant. For the start select the difficulty and topic of the problem you would like to solve. Then click on the 'Generate a problem' button. Good luck!",
-    "start": (
-        "Please take a moment to read the problem statement. Then you can share you initial thoughts and ask any questions you may have. "
-        # "Please use the record button to communicate with me. Try to think out loud and record all you comments and questions as if it is the real interview."
-    ),
-    "end": "The interview has concluded. Thank you for your participation! In a moment I will provide a detailed feedback on your performance.",
+    "intro": "Nice to meet you! I'm your AI interviewer. Click 'Generate a problem' to start.",
+    "start": ("Read the problem statement, share your initial thoughts, and ask questions using the record button."),
+    "end": "Interview complete. Thank you! Feedback will follow shortly.",
     "error": "An error occurred. Please try again.",
 }
