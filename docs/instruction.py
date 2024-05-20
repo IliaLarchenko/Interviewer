6eb8be078f939db4e1629fb7fefc6b84266270c6
Ilia <ilia.larchenko@gmail.com>
2024-05-20 20:26:44 +0700

    Updated demo notice

--- docs/instruction.py
+++ docs/instruction.py
@@ -3,7 +3,7 @@
 instruction = {
     "demo": """
 <span style="color: red;">
-    This service is running in demo mode with limited performance. For a better experience, run the service locally, please refer to the Instruction tab for more details.
+    This service is running in demo mode with limited performance (eg. slow voice recognition). For a better experience, run the service locally, refer to the Instruction tab for more details.
 </span>
     """,
     "introduction": """
